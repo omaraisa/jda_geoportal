1aba9a6863a40070ba7ebf13abcd4ba82570b938
omar@aisa.net.sa <omar-elhadi@live.com>
2024-12-12 07:52:15 +0300

    Update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path'); // Import the path module
 
 dotenv.config();
 
@@ -8,9 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve React static files from the dist directory
+app.use(express.static(path.join(__dirname, '../client/npm/dist')));
+
+// Catch-all route to serve the React app
 app.get('/', (req, res) => {
-    res.send('Welcome to the Enterprise Node.js and React Application');
+    res.sendFile(path.join(__dirname, '../client/npm/dist', 'index.html'));
 });
+
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the Enterprise Node.js and React Application');
+// });
 
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
