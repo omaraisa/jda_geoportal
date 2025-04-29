e4710ec499dac765c0188a012371f3a3054cd4da
Omar Elhag | Namaa <oelhag@jda.gov.sa>
2025-04-29 14:21:53 +0300

    Deployment Files Restructured

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/geoportal",
+    assetPrefix: '/geoportal/',
     // experimental: {
     //     esmExternals: true,
     //   },
