8d4626a2698e31cdabeffdf8a88d60b643ee41f9
Omar Elhag | Namaa <oelhag@jda.gov.sa>
2025-07-31 10:23:38 +0300

    Deployment Preparation

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
-import { verifyAccessToken, verifyRefreshToken } from '@/lib/utils/token';
+import { verifyAccessToken } from '@/lib/utils/token';
 
 const protectedPaths = [
     '/',
