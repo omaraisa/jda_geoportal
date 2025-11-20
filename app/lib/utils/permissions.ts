68fdcb9d6b15d84b9ae3b263fecfceffca895632
Omar Elhag | Namaa <oelhag@jda.gov.sa>
2025-11-20 11:51:07 +0300

    Minor changes

--- app/lib/utils/permissions.ts
+++ app/lib/utils/permissions.ts
@@ -1,6 +1,7 @@
 export const restrictedPermissions: Record<string, string[]> = {
     UploadLayer: ["editor", "admin"],
     ExportLayer: ["editor", "admin"], 
+    SpatialAnalysis: ["editor", "admin"], 
     AddLayer: ["editor", "admin"],
     EditorComponent: ["editor", "admin"]
 };
